c9b987e7ba58c36243ac4db2ab1e7aed8174de4c
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-13 20:30:00 +0000

    ~

---
+++ api/movies/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+const moviesPath = './../../data/movies.json';
+const movies = require(moviesPath);
+//const writeDataOfMovies = require('./../../utils/writeDataOfMovies.js');
+
+router.get('/', (require, response) => response.send(movies));
+
+router.post('/', (require, response) => {
+      const newMovie = require.body;
+      const newMovieAlredyExists = movies.find(movie => movie.name === newMovie.name);
+      if (newMovieAlredyExists) {
+            response.send(`Error: The movie ${newMovie.name} alredy exists`);
+      } else { 
+            movies.push(newMovie);
+            writeDataOfMovies(moviesPath, movies);
+      }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 const express = require('express'),
       bodyParser = require('body-parser');
 
-const writeDataOfMovies = require('./utils/writeDataOfMovies.js');
-
-const moviesPath = './data/movies.json';
-const movies = require(moviesPath);
+//const writeDataOfMovies = require('./utils/writeDataOfMovies.js');
+const moviesRouter = require('./api/movies/index.js'); 
 
 const port = 3000;
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (require,response) => response.send(`Server Working!`));
-app.get('/movies', (require, response) => response.send(movies));   
 
-app.post('/movies/', (require, response) => {
-      const newMovie = require.body;
-      const newMovieAlredyExists = movies.find(movie => movie.name === newMovie.name);
-      if (newMovieAlredyExists) {
-            response.send(`Error: The movie ${newMovie.name} alredy exists`);
-      } else { 
-            movies.push(newMovie);
-            writeDataOfMovies(moviesPath, movies);
-      }
-});
+app.use('/movies', moviesRouter);   
+// app.post('/movies', moviesRouter);
 
 app.listen(port, () => console.log(`Server running an localhost:${port}`)); 
