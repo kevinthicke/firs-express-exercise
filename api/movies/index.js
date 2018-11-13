169a7b7fbc5abcdedb9d5bddfc7e0bf1003560b5
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-13 20:36:22 +0000

    ~

--- api/movies/index.js
+++ api/movies/index.js
@@ -4,13 +4,14 @@ const router = express.Router();
 
 const moviesPath = './../../data/movies.json';
 const movies = require(moviesPath);
-//const writeDataOfMovies = require('./../../utils/writeDataOfMovies.js');
+const writeDataOfMovies = require('./../../utils/writeDataOfMovies.js');
 
 router.get('/', (require, response) => response.send(movies));
 
 router.post('/', (require, response) => {
       const newMovie = require.body;
       const newMovieAlredyExists = movies.find(movie => movie.name === newMovie.name);
+      
       if (newMovieAlredyExists) {
             response.send(`Error: The movie ${newMovie.name} alredy exists`);
       } else { 
