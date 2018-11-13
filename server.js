06b12bfa90cb4a2f9ba0557e75521d6553289eab
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-13 19:05:21 +0000

    Añadida funcionalidad de escribir fichero

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      fs = require('fs');
 
 const movies = require('./data/movies.json')
 
@@ -9,11 +10,12 @@ app.use(bodyParser.json());
 
 app.get('/', (require,response) => response.send(`Server Working!`));
 app.get('/movies', (require, response) => response.send(movies));   
+
 app.post('/movies/', (require, response) => {
       const newMovie = require.body;
       if (newMovie) {
-            response.send(newMovie);
             movies.push(newMovie);
+            fs.writeFile('texto.json', JSON.stringify(movies), err => console.log('An error has ocurred'));
       } else response.send('Error');
 });
 
